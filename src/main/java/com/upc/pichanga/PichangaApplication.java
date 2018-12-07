55275260008d6051dbcb2ec30e37bd5bdb70af0c
enriqueRS17 <enrique1789@hotmail.com>
2018-12-07 12:37:08 -0500

    inicio de proyecto

---
+++ src/main/java/com/upc/pichanga/PichangaApplication.java
@@ -0,0 +1,12 @@
+package com.upc.pichanga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PichangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PichangaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/upc/pichanga/PichangaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.upc.pichanga;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PichangaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
